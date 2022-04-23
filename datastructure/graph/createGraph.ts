3a13bde4503871eda637913fe6855e4cb40ec668
ahqrt <1217223113@qq.com>
2022-04-23 14:32:11 +0800

    feat: 添加图的初始化算法

--- datastructure/graph/createGraph.ts
+++ datastructure/graph/createGraph.ts
@@ -2,15 +2,15 @@
  * 图的点
  */
 export class Node {
-  public value: number;
-  public in: number;
-  public out: number;
-  public nexts: Set<Node> = new Set();
-  public edges: Set<Node> = new Set();
+  public value: number
+  public in: number
+  public out: number
+  public nexts: Set<Node> = new Set()
+  public edges: Set<Edge> = new Set()
   constructor(value: number) {
-    this.value = value;
-    this.in = 0;
-    this.out = 0;
+    this.value = value
+    this.in = 0
+    this.out = 0
   }
 }
 
@@ -18,27 +18,50 @@ export class Node {
  * 图的边
  */
 export class Edge {
-  public from: Node;
-  public to: Node;
-  public weight: number;
+  public from: Node
+  public to: Node
+  public weight: number
   constructor(weight: number, from: Node, to: Node) {
-    this.weight = weight;
-    this.from = from;
-    this.to = to;
+    this.weight = weight
+    this.from = from
+    this.to = to
   }
 }
 
 export class Graph {
-  nodes: Set<Node>;
-  edges: Set<Edge>;
+  nodes: Map<number, Node>
+  edges: Set<Edge>
   constructor() {
-    this.nodes = new Set();
-    this.edges = new Set();
+    this.nodes = new Map()
+    this.edges = new Set()
   }
 }
 
 
 export function createGraph(matrix: number[][]) {
   const graph = new Graph()
+  matrix.forEach((row, i) => {
+    const form = row[0]
+    const to = row[1]
+    const weight = row[2]
 
+    if (!graph.nodes.has(form)) {
+      graph.nodes.set(form, new Node(form))
+    }
+
+    if (!graph.nodes.has(to)) {
+      graph.nodes.set(to, new Node(to))
+    }
+
+    const fromNode = graph.nodes.get(form)
+    const toNode = graph.nodes.get(to)
+    const newEdge = new Edge(weight, fromNode, toNode)
+    fromNode.nexts.add(toNode)
+    fromNode.out++
+    toNode.in++
+    fromNode.edges.add(newEdge)
+    graph.edges.add(newEdge)
+  })
+
+  return graph
 }
