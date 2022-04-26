f85e7663cd4bd3366017a14172c90413abd325af
ahqrt <1217223113@qq.com>
2022-04-26 21:58:35 +0800

    feat: 小和问题

--- datastructure/sort/mergeSort.ts
+++ datastructure/sort/mergeSort.ts
@@ -53,3 +53,52 @@ function merge(arr: number[], l: number, mid: number, r: number) {
   }
 }
 
+
+/**
+ * 小和问题，可以用归并排序的思想，降低时间复杂度
+ */
+
+export function smallSum(arr: number[]) {
+  if (!arr || arr.length < 2) {
+    return arr
+  }
+  return processSum(arr, 0, arr.length - 1)
+}
+
+function processSum(arr: number[], l: number, r: number) {
+
+  if (l === r) {
+    return 0
+  }
+
+  const mid = l + ((r - l) >> 1)
+  return processSum(arr, l, mid) +
+    processSum(arr, mid + 1, r) +
+    mergeSum(arr, l, mid, r)
+}
+
+function mergeSum(arr: number[], l: number, mid: number, r: number) {
+  const help: number[] = []
+  let i = 0;
+  let p1 = l;
+  let p2 = mid + 1;
+  let res = 0
+
+  while (p1 <= mid && p2 <= r) {
+    res += arr[p1] < arr[p2] ? (r - p2 + 1) * arr[p1] : 0
+    help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++]
+  }
+
+  while (p1 <= mid) {
+    help[i++] = arr[p1++]
+  }
+
+  while (p2 <= r) {
+    help[i++] = arr[p2++]
+  }
+
+  for (let i = 0; i < help.length; i++) {
+    arr[l + i] = help[i]
+  }
+  return res
+}
--- datastructure/test/mergeSort.test.ts
+++ datastructure/test/mergeSort.test.ts
@@ -1,9 +1,16 @@
 import { describe, it, expect } from 'vitest';
-import { mergeSort } from '../sort/mergeSort';
+import { mergeSort, smallSum } from '../sort/mergeSort';
 describe('test mergeSort', () => {
-  it('test', () => {
+  it('test mergeSort is right', () => {
     const arr = [1, 2, 1, 1111, 122, 439, 1293, 129312, 12391239, 12383, 145, 3, 67, 899]
     const realRes = arr.sort((a, b) => a - b)
     expect(arr).toStrictEqual(realRes)
   })
+
+  it('test smallSum', () => {
+    const arr = [1, 2, 3]
+    const res = smallSum(arr)
+    expect(res).toBe(4)
+  })
 })
+
